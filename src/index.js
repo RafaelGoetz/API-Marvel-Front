dc11aa0f04cc93a8a03b4e115f8ed6c1b0897291
RafaelGoetz <rafagoetz@gmail.com>
2021-01-19 23:49:48 -0300

    novo

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
+import axios from 'axios'
 import App from "./app";
+
+const url = 'https://api-marvel-growdev-rafael.herokuapp.com/characters';
+
+axios.get(url)
+    .then(response => console.log(response.data))
+    .catch(error => console.log(error));
 
 const app = new App();
 app.getCharacters();
